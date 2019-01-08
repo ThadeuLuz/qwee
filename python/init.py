c3b8e0765a6c84b4f48bd4fa77670ae776aaca9f
Thadeu Luz <thadeuluz@gmail.com>
2019-01-07 22:52:47 -0300

    all servos

--- python/init.py
+++ python/init.py
@@ -5,21 +5,20 @@ MAX_ANGLE = 35  # Max e min
 
 servo_l = AngularServo(4, min_angle=-MAX_ANGLE, max_angle=MAX_ANGLE)  # 7
 servo_r = AngularServo(17, min_angle=-MAX_ANGLE, max_angle=MAX_ANGLE)  # 11
-# servo_f = AngularServo(27, min_angle=-MAX_ANGLE, max_angle=MAX_ANGLE)  # 13
-# servo_b = AngularServo(22, min_angle=-MAX_ANGLE, max_angle=MAX_ANGLE)  # 15
+servo_f = AngularServo(27, min_angle=-MAX_ANGLE, max_angle=MAX_ANGLE)  # 13
+servo_b = AngularServo(22, min_angle=-MAX_ANGLE, max_angle=MAX_ANGLE)  # 15
 
 
 def set_all(angle=0):
     print(angle)
     global servo_l
     global servo_r
-    #     global servo_f
-    #     global servo_b
-    #     print(angle)
+    global servo_f
+    global servo_b
     servo_l.angle = angle
     servo_r.angle = angle
-    #     servo_f.angle = angle
-    #     servo_b.angle = angle
+    servo_f.angle = angle
+    servo_b.angle = angle
 
 
 while True:
