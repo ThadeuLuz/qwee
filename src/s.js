66c6b96235376a82a243216e388788294e297b1a
Thadeu Luz <thadeuluz@gmail.com>
2019-02-18 09:50:11 -0300

    Simplify test

--- src/s.js
+++ src/s.js
@@ -1,16 +1,10 @@
 const five = require("johnny-five");
 const Raspi = require("raspi-io");
 const board = new five.Board({
-  io: new Raspi(),
-  enableSoftPwm: true
+  io: new Raspi()
 });
 
 board.on("ready", function() {
   const servo = new five.Servo("P1-16");
-
-  this.repl.inject({
-    servo: servo
-  });
-
   servo.sweep();
 });
