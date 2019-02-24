b555ff80985b4f3f209e7a9b96baa2c03163e1a3
Thadeu Luz <thadeuluz@gmail.com>
2019-02-23 23:40:27 -0300

    Auto push

--- dist/sensors/IMU.js
+++ dist/sensors/IMU.js
@@ -8,10 +8,14 @@ var i2c_mpu6050_1 = __importDefault(require("i2c-mpu6050"));
 var address = 0x68;
 var i2c1 = i2c_bus_1["default"].openSync(1);
 var sensor = new i2c_mpu6050_1["default"](i2c1, address);
-var data = sensor.readSync();
 var IMU = function () {
     setInterval(function () {
-        console.log(data);
+        sensor.read(function (err, data) {
+            if (err) {
+                throw err;
+            }
+            console.log(data);
+        });
     }, 100);
 };
 exports["default"] = IMU;
--- src/sensors/IMU.ts
+++ src/sensors/IMU.ts
@@ -4,11 +4,15 @@ import MPU6050 from "i2c-mpu6050";
 const address = 0x68;
 const i2c1 = i2c.openSync(1);
 const sensor = new MPU6050(i2c1, address);
-const data = sensor.readSync();
 
 const IMU = () => {
   setInterval(() => {
-    console.log(data);
+    sensor.read(function(err, data) {
+      if (err) {
+        throw err;
+      }
+      console.log(data);
+    });
   }, 100);
 };
 
