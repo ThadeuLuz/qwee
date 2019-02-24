3f7a9b8f0a6bb2422eebaf454e1e7a25a9b51fd1
Thadeu Luz <thadeuluz@gmail.com>
2019-02-23 23:34:02 -0300

    Auto push

--- dist/sensors/IMU.js
+++ dist/sensors/IMU.js
@@ -1,34 +1,17 @@
 "use strict";
-// import i2c from "i2c-bus";
-// import MPU6050 from "i2c-mpu6050";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 exports.__esModule = true;
-// var mpu6050 = require('mpu6050');
-// @ts-ignore
-var mpu6050_1 = __importDefault(require("mpu6050"));
+var i2c_bus_1 = __importDefault(require("i2c-bus"));
+var i2c_mpu6050_1 = __importDefault(require("i2c-mpu6050"));
+var address = 0x68;
+var i2c1 = i2c_bus_1["default"].openSync(1);
+var sensor = new i2c_mpu6050_1["default"](i2c1);
+var data = sensor.readSync();
 var IMU = function () {
-    // Instantiate and initialize.
-    var mpu = new mpu6050_1["default"]();
-    mpu.initialize();
-    // Test the connection before using.
-    mpu.testConnection(function (err, testPassed) {
-        if (testPassed) {
-            mpu.getMotion6(function (err, data) {
-                console.log(data);
-            });
-            // Put the MPU6050 back to sleep.
-            mpu.setSleepEnabled(1);
-        }
-    });
-    // ---------
-    // const address = 0x68;
-    // const i2c1 = i2c.openSync(1);
-    // const sensor = new MPU6050(i2c1);
-    // const data = sensor.readSync();
-    // setInterval(() => {
-    //   console.log(data);
-    // }, 100);
+    setInterval(function () {
+        console.log(data);
+    }, 100);
 };
 exports["default"] = IMU;
--- src/sensors/IMU.ts
+++ src/sensors/IMU.ts
@@ -1,35 +1,15 @@
-// import i2c from "i2c-bus";
-// import MPU6050 from "i2c-mpu6050";
+import i2c from "i2c-bus";
+import MPU6050 from "i2c-mpu6050";
 
-// var mpu6050 = require('mpu6050');
-// @ts-ignore
-import mpu6050 from "mpu6050";
+const address = 0x68;
+const i2c1 = i2c.openSync(1);
+const sensor = new MPU6050(i2c1);
+const data = sensor.readSync();
 
 const IMU = () => {
-  // Instantiate and initialize.
-  const mpu = new mpu6050();
-  mpu.initialize();
-
-  // Test the connection before using.
-  mpu.testConnection(function(err, testPassed) {
-    if (testPassed) {
-      mpu.getMotion6(function(err, data) {
-        console.log(data);
-      });
-      // Put the MPU6050 back to sleep.
-      mpu.setSleepEnabled(1);
-    }
-  });
-
-  // ---------
-
-  // const address = 0x68;
-  // const i2c1 = i2c.openSync(1);
-  // const sensor = new MPU6050(i2c1);
-  // const data = sensor.readSync();
-  // setInterval(() => {
-  //   console.log(data);
-  // }, 100);
+  setInterval(() => {
+    console.log(data);
+  }, 100);
 };
 
 export default IMU;
