f4398f817183fcc81fab1d25b5693dee03223605
Thadeu Luz <thadeuluz@gmail.com>
2019-02-23 23:52:18 -0300

    Auto push

--- dist/sensors/IMU.js
+++ dist/sensors/IMU.js
@@ -5,18 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 exports.__esModule = true;
 var i2c_bus_1 = __importDefault(require("i2c-bus"));
 var i2c_mpu6050_1 = __importDefault(require("i2c-mpu6050"));
-var address = 0x68;
-var i2c1 = i2c_bus_1["default"].openSync(1);
-var sensor = new i2c_mpu6050_1["default"](i2c1, address);
 var IMU = function () {
-    setInterval(function () {
-        sensor.read(function (err, data) {
-            // if (err) {
-            // throw err;
-            // }
-            // console.clear();
-            console.log(data);
-        });
-    }, 100);
+    try {
+        var address = 0x68;
+        var i2c1 = i2c_bus_1["default"].openSync(1);
+        var sensor_1 = new i2c_mpu6050_1["default"](i2c1, address);
+        setInterval(function () {
+            sensor_1.read(function (err, data) {
+                // if (err) {
+                // throw err;
+                // }
+                // console.clear();
+                console.log(data);
+            });
+        }, 100);
+    }
+    catch (err) {
+        console.log("imu error");
+    }
 };
 exports["default"] = IMU;
--- src/sensors/IMU.ts
+++ src/sensors/IMU.ts
@@ -1,20 +1,24 @@
 import i2c from "i2c-bus";
 import MPU6050 from "i2c-mpu6050";
 
-const address = 0x68;
-const i2c1 = i2c.openSync(1);
-const sensor = new MPU6050(i2c1, address);
-
 const IMU = () => {
-  setInterval(() => {
-    sensor.read(function(err, data) {
-      // if (err) {
-      // throw err;
-      // }
-      // console.clear();
-      console.log(data);
-    });
-  }, 100);
+  try {
+    const address = 0x68;
+    const i2c1 = i2c.openSync(1);
+    const sensor = new MPU6050(i2c1, address);
+
+    setInterval(() => {
+      sensor.read(function(err, data) {
+        // if (err) {
+        // throw err;
+        // }
+        // console.clear();
+        console.log(data);
+      });
+    }, 100);
+  } catch (err) {
+    console.log("imu error");
+  }
 };
 
 export default IMU;
